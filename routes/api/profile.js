675d703ab7f851801b4a98c2019026623d18f65b
Mobeen Abdullah <mobeenabdullah@gmail.com>
2020-11-18 02:05:54 +0500

    created route for deleting profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -133,4 +133,27 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo - remove users posts
+
+        // Remove Profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
