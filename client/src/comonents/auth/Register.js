e7f61b6575a6993ca9d90c479452ad72f81cc7cd
Mobeen Abdullah <mobeenabdullah@gmail.com>
2020-11-20 12:42:57 +0500

    worked on Login and register form settings

--- client/src/comonents/auth/Register.js
+++ client/src/comonents/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
 
@@ -13,12 +14,12 @@ const Register = () => {
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
             console.log('Passwords do not match');
         } else {
-            console.log(formData);
+            console.log('SUCCESS');
         }
     }
 
@@ -27,14 +28,11 @@ const Register = () => {
         <p className='lead'><i className='fas fa-user'></i> Create Your Account</p>
         <form className='form' onSubmit={e => onSubmit(e)}>
             <div className='form-group'>
-            <input type='text' placeholder='Name' name='name' value={name} onChange={e => onChange(e)} required />
+                <input type='text' placeholder='Name' name='name' value={name} onChange={e => onChange(e)} required />
             </div>
             <div className='form-group'>
-            <input type='email' placeholder='Email Address' value={email} onChange={e => onChange(e)} name='email' />
-            <small className='form-text'
-                >This site uses Gravatar so if you want a profile image, use a
-                Gravatar email</small
-            >
+                <input type='email' placeholder='Email Address' value={email} onChange={e => onChange(e)} name='email' />
+            <small className='form-text'>This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
             </div>
             <div className='form-group'>
                 <input
@@ -43,23 +41,23 @@ const Register = () => {
                     name='password'
                     value={password}
                     onChange={e => onChange(e)}
-                minLength='6'
-            />
+                    minLength='6'
+                />
             </div>
             <div className='form-group'>
-            <input
-                type='password'
-                placeholder='Confirm Password'
+                <input
+                    type='password'
+                    placeholder='Confirm Password'
                     name='password2'
                     value={password2}
                     onChange={e => onChange(e)}
-                minLength='6'
-            />
+                    minLength='6'
+                />
             </div>
             <input type='submit' className='btn btn-primary' value='Register' />
         </form>
         <p className='my-1'>
-            Already have an account? <a href='login.html'>Sign In</a>
+            Already have an account? <Link to='/login'>Sign In</Link>
         </p>
     </Fragment>
 }
