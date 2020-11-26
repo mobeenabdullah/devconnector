41ff5fca8c87e57a651b56d21339c53143a16e5b
Mobeen Abdullah <mobeenabdullah@gmail.com>
2020-11-26 21:58:34 +0500

    listing education and experiences on dashboard

--- client/src/comonents/dashboard/Dashboard.js
+++ client/src/comonents/dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
@@ -19,6 +21,8 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
         </p>
         {profile !== null ? <Fragment>
             <DashboardActions />
+            <Experience experience={profile.experience} />
+            <Education education={profile.education} />
         </Fragment> : <Fragment>
             <p>You have not yet setup a profile, please add some info</p>
             <Link to='/create-profile' className='btn btn-primary my-1'>
