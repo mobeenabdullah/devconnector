b211caa427ac78f5e96e0236a8f0b359611570b5
Mobeen Abdullah <mobeenabdullah@gmail.com>
2020-12-05 12:36:24 +0500

    deleting posts functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import { set } from 'mongoose';
 import { setAlert } from './alert';
 import {
     GET_POSTS,
     POST_ERROR,
-    UPDATE_LIKES
+    UPDATE_LIKES,
+    DELETE_POST
 } from './types';
 
 // Get posts
@@ -54,6 +56,27 @@ export const removeLike = id => async dispatch => {
             type: UPDATE_LIKES,
             payload: { id, likes: res.data }
         });
+
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+}
+
+// Delete post
+export const deletePost = id => async dispatch => {
+    try {
+        
+        const res = await axios.delete(`/api/posts/${id}`);
+
+        dispatch({
+            type: DELETE_POST,
+            payload: id
+        });
+
+        dispatch(setAlert('Post Removed', 'success'));
 
     } catch (err) {
         dispatch({
