4fc13ad60bd6380150aada479994f40b1778cc20
Mobeen Abdullah <mobeenabdullah@gmail.com>
2020-12-07 18:03:59 +0500

    adding comments functionality

--- client/src/comonents/post/Post.js
+++ client/src/comonents/post/Post.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -15,6 +16,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     return loading || post === null ? <Spinner /> : <Fragment>
         <Link to='/posts' className='btn'>Back To Posts</Link>
         <PostItem post={post} showActions={false} />
+        <CommentForm postId={post._id} />
     </Fragment>
 }
 
